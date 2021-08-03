7e5b5ad3744db665500fa8bca63c3a23d6f5119d
soul.shao@aax.com <soul.shao>
2021-08-03 22:20:34 +0800

    support upload

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const path = require('path')
 const querystring = require('querystring')
 const xlsx = require('node-xlsx')
 const Pug = require('koa-pug')
+const mime = require('mime-types')
+const KoaStatic = require('koa-static')
+const koaBody = require('koa-body')({multipart: true, uploadDir: './uploads'})
 
 const pug = new Pug({
     viewPath: path.resolve(__dirname, './views'),
@@ -14,20 +17,30 @@ const pug = new Pug({
 
 const homeContent = fs.readFileSync(path.join(__dirname, './index.html'));
 const errorContent = fs.readFileSync(path.join(__dirname, './error.html'));
-const excelPath = fs.readFileSync(path.join(__dirname, './data.xlsx'));
+const uploadCpntent = fs.readFileSync(path.join(__dirname, './upload.html'));
+const excelPath = fs.readFileSync(path.join(__dirname, './uploads/data.xlsx'));
 
-const data = xlsx.parse(excelPath)[0].data;
-const transData = new Map
-const label = data[0]
-data.forEach((item, index) => {
-    if (index > 0) {
-        const data = label.reduce(function (a, b, index) {
-            a[b] = item[index]
-            return a
-        }, {})
-        transData.set(`${item[0]}:${item[1]}`, data)
+let transData = null
+
+function parseExcel(excelPath) {
+    try {
+        const newTransData = new Map
+        const data = xlsx.parse(excelPath)[0].data;
+        const label = data[0]
+        data.forEach((item, index) => {
+            if (index > 0) {
+                const data = label.reduce(function (a, b, index) {
+                    a[b] = item[index]
+                    return a
+                }, {})
+                newTransData.set(`${item[0]}:${item[1]}`, data)
+            }
+        })
+        return newTransData
+    } catch(err) {
+        throw err
     }
-})
+}
 
 function getFormData(ctx) {
     // 获取数据 异步
@@ -49,6 +62,7 @@ function getFormData(ctx) {
 
 router.post('/grade', async (ctx, next) => {
     try {
+        if (transData === null) transData = parseExcel(excelPath)
         const data = await getFormData(ctx)
         const { username, phone } = querystring.parse(data)
         const key = `${username}:${phone}`
@@ -66,10 +80,47 @@ router.post('/grade', async (ctx, next) => {
     }
 })
 
+router.post('/uploadFile', koaBody, async (ctx, next) => {
+    try {
+        const { pwd } = ctx.request.body
+        if (pwd === 'rHGtBJL6irdD') {
+            const {path: _path, name, type} = ctx.request.files.excel
+            const fileExtension = mime.extension(type)
+            if (/xlsx/.test(fileExtension)) {
+                const reader = fs.createReadStream(_path)
+                let filePath = path.join(__dirname, './uploads') + `/${name.replace(fileExtension, `${+new Date()}.${fileExtension}`)}`;
+                const upStream = fs.createWriteStream(filePath);
+                reader.pipe(upStream).on('finish', () => {
+                    transData = parseExcel(filePath)
+                });
+                await ctx.render('result', {
+                    success: true
+                }, true)
+            } else {
+                throw new Error('真的离谱，上传文件格式错误')
+            }
+        } else {
+            throw new Error('你踏马密码错误')
+        }
+    } catch (err) {
+        await ctx.render('result', {
+            success: false,
+            reason: err.message
+        }, true)
+    }
+})
+
 router.get('/', async (ctx, next) => {
     ctx.type = "html";
     ctx.body = homeContent;
 })
+
+router.get('/upload', async (ctx, next) => {
+    ctx.type = "html";
+    ctx.body = uploadCpntent;
+})
+
+app.use(KoaStatic(path.join(__dirname, './images')));
 
 app.use(router.routes());
 
